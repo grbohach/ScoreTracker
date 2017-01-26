4dae25a673b1f35f4e5a63cfcffe1ed87c371572
grbohach <grbohach@coe.edu>
2017-01-26 10:36:44 -0600

    Simple score tracker. Can add one to each score.

--- app/src/main/java/com/example/user/scoretracker/MainActivity.java
+++ app/src/main/java/com/example/user/scoretracker/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.user.scoretracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.user.scoretracker.R.id.home;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button home;
+    private Button away;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupButtons();
+    }
+
+    private void setupButtons(){
+        home = (Button) this.findViewById(R.id.btnHome);
+        home.setOnClickListener(this);
+        away = (Button) this.findViewById(R.id.btnAway);
+        away.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.btnHome:
+                String sHome = home.getText().toString();
+                int iHome = Integer.parseInt(sHome) + 1;
+                home.setText(Integer.toString(iHome));
+                break;
+            case R.id.btnAway:
+                String sAway = away.getText().toString();
+                int iAway = Integer.parseInt(sAway) + 1;
+                away.setText(Integer.toString(iAway));
+                break;
+        }
     }
 }
