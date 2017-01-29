f2d11c06cbbfb098b90e8e124287486faafd5b2a
grbohach <grbohach@coe.edu>
2017-01-28 18:55:27 -0600

    Menu options added to the action bar of the app. Reset and decrement have been added as submenu actions.

--- app/src/main/java/com/example/user/scoretracker/MainActivity.java
+++ app/src/main/java/com/example/user/scoretracker/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.user.scoretracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import static com.example.user.scoretracker.R.id.home;
 
@@ -17,6 +21,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupButtons();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ResetButton:
+                Toast.makeText(this, "Reset Scores", Toast.LENGTH_SHORT).show();
+                home.setText("0");
+                away.setText("0");
+                break;
+            case R.id.decreaseHome:
+                Toast.makeText(this, "Home score reduced by 1", Toast.LENGTH_SHORT).show();
+                int oldScore = Integer.parseInt(home.getText().toString());
+                oldScore--;
+                home.setText(Integer.toString(oldScore));
+                break;
+            case R.id.decreaseAway:
+                Toast.makeText(this, "Away score reduced by 1", Toast.LENGTH_SHORT).show();
+                oldScore = Integer.parseInt(away.getText().toString());
+                oldScore--;
+                away.setText(Integer.toString(oldScore));
+                break;
+        }
+
+        return true;
     }
 
     private void setupButtons(){
